04bd5220911a02054a36cc7214ae0904d687c74c
AbhilashDash10082k4 <abhilash10082k4@gmail.com>
2024-12-25 18:53:29 +0530

    refresh token for refreshing access to user's accounts

--- slide/src/lib/refresh.ts
+++ slide/src/lib/refresh.ts
@@ -1,6 +1,17 @@
 import axios from 'axios'
+
+//The purpose of the function is to refresh an Instagram access token. Instagram access tokens expire after a set period, and this API endpoint extends the token's validity.
+
+//Often used in applications that integrate with Instagram to ensure continuous access to user data without requiring frequent re-authentication.
+
+//refresh_access_tokenThis endpoint is part of the Instagram Graph API's Basic Display API. It is used to refresh short-lived access tokens, turning them into long-lived access tokens.
+
+/*Query params = grant_type=ig_refresh_token: Specifies the grant type for refreshing the token.
+access_token=${token}: Passes the current access token to the API.
+
+The function returns the data field from the API response.
+The API response typically includes the new access token and its expiration time.*/
 export const refreshToken = async (token: string) => {
-    //in order to update integrations refresh token is reqd
     const refresh_token = await axios.get(`${process.env.INSTAGRAM_BASE_URL}/refresh_access_token?grant_type=ig_refresh_token&access_token=${token}`)
 
     return refresh_token.data;
